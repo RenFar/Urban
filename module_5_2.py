80c4b9840a728bcbbd8937dc88686f50f3dd35d6
RenFar <gabzon84@gmail.com>
2024-10-29 01:20:42 +0500

    проверка

--- module_5_2.py
+++ module_5_2.py
@@ -1,1 +1,11 @@
-111
+def getmatrix(n, m, value):
+    matrix=[]
+    for i in range(n):
+        row=[]
+        matrix.append(row)
+        for j in range(m):
+            row.append(value)
+
+    return matrix
+print(getmatrix(2, 2, 10))
+print(range(12))
