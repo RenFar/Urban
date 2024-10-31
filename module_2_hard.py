9efdc5c0f51ad6d6f211e82e9c7a6a4eae560c45
RenFar <gabzon84@gmail.com>
2024-11-01 01:36:28 +0500

    доработка

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,28 +1,30 @@
 
 n = input('Введите число от 3 до 20: ')
 n = int(n)
+if n < 3 or n >20:
+    print("!!!Вы ввели неверное число!!!")
+else:
+    def podbor(n):
+        deliteli = []
+        for i in range(2, n+1):
+            if n / i == n // i:
+                deliteli.append(i)
+        password = []
+        for j in range(len(deliteli)):
+            a = 1
+            b = deliteli[j] - 1
 
-def podbor(n):
-    deliteli = []
-    for i in range(2, n+1):
-        if n / i == n // i:
-            deliteli.append(i)
-    password = []
-    for j in range(len(deliteli)):
-        a = 1
-        b = deliteli[j] - 1
+            while a < b:
+                password.append(a)
+                password.append(b)
+                a += 1
+                b -= 1
 
-        while a < b:
-            password.append(a)
-            password.append(b)
-            a += 1
-            b -= 1
-
-    return password
+        return password
 
 
-print((podbor(n)))
-spisok = "".join(map(str, podbor(n)))
+    print((podbor(n)))
+    spisok = "".join(map(str, podbor(n)))
 
-print(f" {n}  = {spisok}")
+    print(f" {n}  = {spisok}")
 
