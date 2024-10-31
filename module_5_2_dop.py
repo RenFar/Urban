7cb029c367ef76d096434eaa19548a0fe1b9caef
RenFar <gabzon84@gmail.com>
2024-10-31 17:06:04 +0500

    доп задание по функции

---
+++ module_5_2_dop.py
@@ -0,0 +1,11 @@
+def get_triangle(n, value):
+    triangle = []
+    for i in range(n):
+        row = []
+        triangle.append(row)
+
+        for j in range(i+1):
+            row.append(value)
+
+    return triangle
+print(get_triangle(5, 10))
