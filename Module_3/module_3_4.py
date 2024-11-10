fb7c50071496684cf0b4962d14a182c1a188727a
RenFar <gabzon84@gmail.com>
2024-11-10 21:33:40 +0500

    ответ

---
+++ Module_3/module_3_4.py
@@ -0,0 +1,20 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_low = root_word.lower()
+    other_words = list(other_words)
+
+    for i in other_words:
+        other_low = i.lower()
+        find_root_in_other = other_low.find(root_low)
+        if find_root_in_other != -1:
+            same_words.append(i)
+    for j in other_words:
+        other_low_2 = j.lower()
+        find_other_in_root = root_low.find(other_low_2)
+        if find_other_in_root != -1:
+            same_words.append(j)
+
+    return same_words
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
