2a1241ba91b2cbdc75946519d4138fc4a32cbd47
RenFar <gabzon84@gmail.com>
2024-11-11 16:16:27 +0500

    доработка по добавлению roo_word

--- Module_3/module_3_4.py
+++ Module_3/module_3_4.py
@@ -8,12 +8,14 @@ def single_root_words(root_word, *other_words):
         find_root_in_other = other_low.find(root_low)
         if find_root_in_other != -1:
             same_words.append(i)
+            same_words.append(root_word)
     for j in other_words:
         other_low_2 = j.lower()
         find_other_in_root = root_low.find(other_low_2)
         if find_other_in_root != -1:
             same_words.append(j)
-
+    same_words = set(same_words)
+    same_words = list(same_words)
     return same_words
 
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
