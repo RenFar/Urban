bd08383fc304c96e8b068f3252556c8f8d250f6e
RenFar <gabzon84@gmail.com>
2024-12-25 19:06:14 +0500

    ответ

---
+++ Module_7/module_7_1.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+
+class Product:
+
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight= weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+class Shop:
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+
+    def get_products(self):
+        product = open(self.__file_name, 'r')
+        products_str = product.read()
+        product.close()
+
+        return products_str
+
+    def add(self, *products):
+        new_product = self.get_products()
+        for product_ in products:
+            if product_.name in new_product:
+                print(f"Продукт {product_.name} уже есть в магазине")
+            else:
+
+                with open(self.__file_name, 'a') as file:
+                    file.write(str(product_) + "\n")
+                    new_product += str(product_) + "\n"
+
+
+s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+
+print(p1, "\n")
+print(p2, "\n") # __str__
+print(p3, "\n")
+
+print(s1.get_products(), "...\n")
+
+s1.add(p1)
+print(s1.get_products(), "...\n")
+s1.add(p2)
+print(s1.get_products(), "...\n")
+s1.add(p3)
+
+print(s1.get_products())
