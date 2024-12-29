128f6835f7db751f6b041af0beec7cf4a892adef
RenFar <gabzon84@gmail.com>
2024-12-29 17:01:56 +0500

    ответ

--- Module_6/module_6_1.py
+++ Module_6/module_6_1.py
@@ -1,58 +1,49 @@
-from pprint import pprint
-
-class Product:
-
-    def __init__(self, name, weight, category):
+class Animal:
+    def __init__(self, name):
         self.name = name
-        self.weight= weight
-        self.category = category
+        self.alive = True
+        self.fed = False
 
-    def __str__(self):
-        return f"{self.name}, {self.weight}, {self.category}"
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
-class Shop:
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
 
-    def __init__(self):
-        self.__file_name = 'products.txt'
-        self.__products = []
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+# Пример
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
 
 
-    def get_products(self):
-        product = open(self.__file_name, 'r')
-        products_str = product.read()
-        product.close()
-
-        return products_str
-
-    def add(self, *products):
-
-        for product_ in products:
-            if product_.name in self.__products:
-                print(f"Продукт {product_.name} уже есть в магазине")
-            else:
-
-                with open(self.__file_name, 'a') as file:
-                    file.write(str(product_) + "\n")
-
-s1 = Shop()
-
-p1 = Product('Potato', 50.5, 'Vegetables')
-
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-
-p3 = Product('Potato', 5.5, 'Vegetables')
-
-
-print(p1, "\n")
-print(p2, "\n") # __str__
-print(p3, "\n")
-
-print(s1.get_products(), "...\n")
-
-s1.add(p1)
-print(s1.get_products(), "...\n")
-s1.add(p2)
-print(s1.get_products(), "...\n")
-s1.add(p3)
-
-print(s1.get_products())
