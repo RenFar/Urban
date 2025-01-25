fa514a258ad9719cb9e502fcf8cda2b4d31eb1b5
RenFar <gabzon84@gmail.com>
2025-01-25 17:15:37 +0500

    ответ

--- Module_12/module_12_1.py
+++ Module_12/module_12_1.py
@@ -20,18 +20,18 @@ class Runner:
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         runner = Runner("Иван")
-        for _ in range(10):
+        for i in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
     def test_run(self):
         runner = Runner("Пётр")
-        for _ in range(10):
+        for i in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
     def test_challenge(self):
         runner1 = Runner("Илья")
         runner2 = Runner("Семён")
-        for _ in range(10):
+        for i in range(10):
             runner1.walk()
             runner2.run()
         self.assertNotEqual(runner1.distance, runner2.distance)
